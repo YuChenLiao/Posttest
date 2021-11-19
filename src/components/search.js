f2f6392bf5688b9f88eaccc45175e8e0a480d638
YuChenLiao <yuchenliao3@gmail.com>
2021-11-19 10:03:16 +0800

    添加顶部搜索栏demo“

---
+++ src/components/search.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import style from './search.css';
+
+const SearchInput = () => {
+  return (
+    <div className={style.header}></div>
+  )
+}
+
+export default SearchInput;
--- src/routes/IndexPage.js
+++ src/routes/IndexPage.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { connect } from 'dva';
 import styles from './IndexPage.css';
+import SearchInput from '../components/search';
 
 function IndexPage() {
   return (
     <div className={styles.back}>
+      <SearchInput></SearchInput>
     </div>
   );
 }
