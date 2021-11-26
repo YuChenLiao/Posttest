98719b7b85bcf297fd1cd3b379ca9089aa74875c
YuChenLiao <yuchenliao3@gmail.com>
2021-11-26 09:40:37 +0800

    开始构建主要界面

--- src/components/mainPages.js
+++ src/components/mainPages.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import style from './mainPages.css'
 
-const MainPage = () => {
+const MainPage = (any) => {
+  console.log(any);
+
   return (
     <div className={style.main}>主要内容展示区域</div>
   )
--- src/routes/IndexPage.js
+++ src/routes/IndexPage.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React,{ useState } from 'react';
 import { connect } from 'dva';
 import styles from './IndexPage.css';
 import SearchInput from '../components/search';
@@ -6,11 +6,17 @@ import HeadTab from '../components/headTab';
 import MainPage from '../components/mainPages';
 
 function IndexPage() {
+  const [page, setPage] = useState('1');
+  
+  const changePage = (value) => {
+    setPage(value);
+  }
+
   return (
     <div className={styles.back}>
       <HeadTab></HeadTab>
       <SearchInput></SearchInput>
-      <MainPage></MainPage>
+      <MainPage page={page} changePage={changePage}></MainPage>
     </div>
   );
 }
