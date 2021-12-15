d744e3cda111b232e22227edee03d377b78ac846
YuChenLiao <yuchenliao3@gmail.com>
2021-12-15 10:51:02 +0800

    完成导航栏样式

--- src/components/headTab.js
+++ src/components/headTab.js
@@ -4,6 +4,12 @@ import style from './headTab.css';
 const HeadTab = () => {
   return (
     <div className={style.sheader}>
+      <div className={style.tabContent}>
+        <span>1</span>
+        <span>2</span>
+        <span>3</span>
+        <span className={style.tRight}>4</span>
+      </div>
     </div>
   )
 }
