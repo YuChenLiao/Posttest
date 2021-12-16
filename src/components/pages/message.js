ae36e6ce7064e9840e4d4e3a8bcef6a63c3672b9
YuChenLiao <yuchenliao3@gmail.com>
2021-12-16 17:18:26 +0800

    完成信息页面初步样式

--- src/components/pages/message.js
+++ src/components/pages/message.js
@@ -1,8 +1,32 @@
 import React from "react";
+import { List } from 'antd'
+import 'antd/dist/antd.css';
+import style from "./message.css"
+
+const data = [
+  '消息1',
+  '消息2',
+  '消息3',
+  '消息4',
+  '消息5',
+  '消息6'
+]
 
 const MessagePage = () => {
   return (
-    <div>这是信息页</div>
+    <div className={style.mBox}>
+      <div className={style.mContent}>
+        <List
+          bordered
+          dataSource={data}
+          renderItem={item => (
+            <List.Item>
+              {item}
+            </List.Item>
+          )}
+        />
+      </div>
+    </div>
   )
 }
 
