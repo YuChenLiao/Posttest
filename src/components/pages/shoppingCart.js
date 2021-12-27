b86caff1086f69c16b49279f75b9815f0d48e2df
YuChenLiao <yuchenliao3@gmail.com>
2021-12-27 16:10:52 +0800

    完成购物车大致样式

--- src/components/pages/shoppingCart.js
+++ src/components/pages/shoppingCart.js
@@ -1,8 +1,32 @@
 import React from "react";
+import { List } from 'antd';
+import 'antd/dist/antd.css';
+import style from './message.css'
+
+const data = [
+  '商品1',
+  '商品2',
+  '商品3',
+  '商品4',
+  '商品5',
+  '商品6'
+]
 
 const ShoppingCart = () => {
   return (
-    <div>购物车</div>
+    <div className={style.mBox}>
+      <div className={style.mContent}>
+        <List
+          bordered
+          dataSource={data}
+          renderItem={item => (
+            <List.Item>
+              {item}
+            </List.Item>
+          )}
+        />
+      </div>
+    </div>
   )
 }
 
